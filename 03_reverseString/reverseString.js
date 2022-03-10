ffe536a40d8ed1884121d9fe0d980d927ece2eb7
Alex I <alextheclaw@hotmail.com>
2022-03-10 15:26:04 -0800

    Split the string then reversed it then joined it.
    Reversing a string. Make sure you pay attention to the quotations on the .split() part and the .join() part.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let result = "";
+  // find a way to split the string up
+  // then, use the .reverse() function to reverse the array
+  // then, use the .join() function to bring it all together
+  // return the results
+  result = string.split("").reverse().join(""); // this will go from left to right
+  return result;
 };
 
 // Do not edit below this line
