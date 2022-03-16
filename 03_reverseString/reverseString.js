9d725e0f803bb34f6f0ff04ff3bbe3b762380c2f
Alex I <alextheclaw@hotmail.com>
2022-03-16 12:59:21 -0700

    Update reverseString.js
    Didn't need to create the result variable because I was not planning to re-use the string for anything. So removed it and made the code easier to read.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,9 @@
 const reverseString = function (string) {
-  let result = "";
   // find a way to split the string up
   // then, use the .reverse() function to reverse the array
   // then, use the .join() function to bring it all together
   // return the results
-  result = string.split("").reverse().join(""); // this will go from left to right
-  return result;
+  return string.split("").reverse().join(""); // this will go from left to right
 };
 
 // Do not edit below this line
