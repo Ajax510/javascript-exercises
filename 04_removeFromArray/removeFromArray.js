dd4f4e6256ba7d90f8b223a66f90c1c4d3e73a93
Alex I <alextheclaw@hotmail.com>
2022-03-17 15:01:49 -0700

    Refactored entire code, result the same
    Refactored my code and simplified it. Made it easier to read and understand. Added some comments to help explain the process.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,18 @@
-const removeFromArray = function (arr, ...args) {
-  for (let j = 0; j < args.length; j++) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === args[j]) {
-        arr.splice(i, 1);
-      }
+const removeFromArray = function (...args) {
+  // very first item in the list of arguments is the array
+  const array = args[0];
+  // Create a new empty array
+  const newArray = [];
+  // forEach will loop throught the array
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESSS, it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
     }
-  }
-  return arr;
+  });
+  return newArray;
 };
 
 // Do not edit below this line
