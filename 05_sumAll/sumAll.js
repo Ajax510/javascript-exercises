d06f421f1b713cd5b1988d15a05e4ae9b6fdae09
Alex I <alextheclaw@hotmail.com>
2022-03-17 15:11:04 -0700

    Moved some things but added nothing new.
    Code was pretty much left untouched and I got the same results. I have moved some of the if statements around but did not add anything new. Code was very similar to the solutions.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,15 +8,13 @@ const sumAll = function (int1, int2) {
   let smallNum = int1;
   let largeNum = int2;
 
+  // if, any of the vaiables are not a number
+  // return error
+  if (!Number.isInteger(smallNum) || !Number.isInteger(largeNum))
+    return "ERROR";
   // this will check to make sure all variables are greater than 0
-  if (smallNum < 0 || largeNum < 0) {
-    // if they are, return an error
-    return "ERROR";
-    // else if, any of the vaiables are not a number
-  } else if (!Number.isInteger(smallNum) || !Number.isInteger(largeNum)) {
-    // if not a number, return error
-    return "ERROR";
-  }
+  // if they are, return an error
+  if (smallNum < 0 || largeNum < 0) return "ERROR";
 
   // this will make sure the smaller number is set as the starting point
   if (int1 > int2) {
